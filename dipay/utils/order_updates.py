07f03400dca25dd65d948da996331367b20312ad
brankwong <brankwong@qq.com>
2022-12-27 12:52:19 +0800

    修复跟单金额更新的bug ORM字段乘起来再求和问题

--- dipay/utils/order_updates.py
+++ dipay/utils/order_updates.py
@@ -1,15 +1,14 @@
 from dipay.models import Pay2Orders, ApplyOrder
 from django.db.models import Sum
 from decimal import Decimal
+from django.db.models import F
 
 def order_payment_update(order_obj,order_id=None):
     if order_id:
         order_obj = ApplyOrder.objects.filter(pk=order_id).first()
 
-    # order_obj.rcvd_amount = sum([item.amount for item in Pay2Orders.objects.filter(order=order_obj)])
-
-
-    total_rcvd_dict = Pay2Orders.objects.filter(order=order_obj).aggregate(total_rcvd = Sum('amount'))
+    total_rcvd_dict = Pay2Orders.objects.filter(order=order_obj).aggregate(
+        total_rcvd = Sum(F('amount')*F("rate")))
     # total_rcvd_dict {'total_rcvd': Decimal('4600.00')} <class 'dict'>
     print('total_rcvd', total_rcvd_dict,type(total_rcvd_dict))
     # 直接用布尔运算，比条件判断效率更高
