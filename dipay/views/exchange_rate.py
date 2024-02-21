8c6936deae9929c8e7dfe1d4b436f2cf5296e592
brankwong <brankwong@qq.com>
2024-02-22 07:32:22 +0800

    汇率搜索

--- dipay/views/exchange_rate.py
+++ dipay/views/exchange_rate.py
@@ -1,8 +1,18 @@
-import os
-from stark.service.starksite import StarkHandler
+
+from stark.service.starksite import StarkHandler, Option
 from stark.utils.display import PermissionHanlder
+
 
 
 class ExchangeRateHandler(PermissionHanlder, StarkHandler):
     page_title = "汇率"
+
+    search_list = ["update_date","currency__title__icontains"]
+
+    search_placeholder = '搜日期(如2022-01-01）'
+
+    # 加入一个组合筛选框, default是默认筛选的值，必须是字符串
+    option_group = [
+        Option(field='currency', verbose_name="币种" ),
+    ]
 
