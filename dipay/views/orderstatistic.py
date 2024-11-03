4b944fedd73d3011e2cccfad38306959ff77b6ac
brankwong <brankwong@qq.com>
2024-11-03 12:10:35 +0800

    统计看板添加人民币合计

--- dipay/views/orderstatistic.py
+++ dipay/views/orderstatistic.py
@@ -2,6 +2,7 @@ from stark.service.starksite import StarkHandler
 from stark.utils.display import  PermissionHanlder
 from django.shortcuts import HttpResponse, render
 from django.db.models import Sum
+from dipay.models import ExchangeRate
 
 
 class OrderStatisticHandler(PermissionHanlder,StarkHandler):
@@ -24,12 +25,23 @@ class OrderStatisticHandler(PermissionHanlder,StarkHandler):
         total_collect_queryset = model_class.objects.filter(status__in=[2,3],order__order_type__in=[0,1,]).values("order__currency__title").annotate(total_to_collect= Sum("order__collect_amount"))
 
         count = 0
+        total_amount = 0
         for item in total_collect_queryset:
             count += 1
-            currency_label = "金凯{}应收账款".format(item["order__currency__title"])
+            currency_title = item["order__currency__title"]
+            currency_label = "金凯{}应收账款".format(currency_title)
             total_to_collect_amount =format( item.get("total_to_collect"),",f")
             row = {"active":"", "data":[count,currency_label,total_to_collect_amount]}
             data_list.append(row)
+            # sum the total amount in RMB, 1st to get exchange rate.
+            currency_obj = ExchangeRate.objects.filter(currency__title=currency_title).order_by("-id").first()
+            # RMB has no exchange record.
+            exchang_rate = currency_obj.rate if currency_obj else 1
+            total_amount += item.get("total_to_collect") * exchang_rate
+
+        # 最后一行加入折合人民币统计
+        row =  {"active":"", "data":["合计"," 折合人民币 ", format(total_amount,",.2f")]}
+        data_list.append(row)
 
         return render(request, "stark/show_list.html", locals())
 
